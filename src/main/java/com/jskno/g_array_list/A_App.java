f10217e65943ec22066568199e41a7c51d8e6165
josecano <jose.cano@europcar.com>
2024-12-28 13:12:06 +0100

    LinkedList

---
+++ src/main/java/com/jskno/g_array_list/A_App.java
@@ -0,0 +1,57 @@
+package com.jskno.g_array_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A_App {
+
+    public static void main(String[] args) {
+
+        // resizing the array takes O(N) --> default size when not indicated is only 10
+        List<String> names = new ArrayList<>();
+        // If we know more or less the size is better to indicate
+        List<String> namesWithSize = new ArrayList<>(5000);
+
+        // adding elements at the end O(1)
+        names.add("Kevin");
+        names.add("Adam");
+        names.add("Jose");
+        names.add("Ana");
+
+        // random indexing in O(1)
+        System.out.println(names.get(2));
+
+        // removing last element in O(1)
+        // No need to shift
+        names.removeLast();
+
+        // ARRAYLIST (ARRAYS) ARE FAST IF WE MANIPULATE THE LAST ITEM OR ACCESS ITEMS BY INDEX
+
+        // insert item into a given index - O(N)
+        // Because under the hood Java shifts all the items
+        names.add(1, "Daniel");
+
+        // remove item from an arbitrary position - O(N)
+        // Java is going to shift every single item > 1 to the left
+        names.remove(1);
+
+        // remove item by its value not index - O(N)
+        // Search + Delete + Shift = O(N) + O(1) + O(N) = O(N)
+        names.remove("Jose");
+
+        // contains will check whether the item is present in the array inspecting one by one
+        // Linear search will take O(N)
+        System.out.println(names.contains("Adam"));
+
+        for (String name: names) {
+            System.out.println(name);
+        }
+
+        Object[] array = names.toArray();
+
+        for (Object o: array) {
+            System.out.println(o);
+        }
+    }
+
+}
---
+++ src/main/java/com/jskno/g_array_list/B_Exercise.java
@@ -0,0 +1,53 @@
+package com.jskno.g_array_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class B_Exercise {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        reverse(list);
+        reverseCourseSolution(list);
+    }
+
+    /*
+    Arrays coding exercise
+    In this lecture, your exercise is to reverse a List<Integer> in-place (without extra memory) - by the way it is included in Java's Collections.
+
+    For example:
+        Input is [1, 2, 3, 4, 5, 6] --> result should be [6, 5, 4, 3, 2, 1] .
+    There are multiple solutions (with quadratic running time as well as with linear running time complexity).
+     */
+    public static void reverse(List<Integer> list) {
+        // here is your implementation
+        System.out.println(list);
+
+        int lastIndex = list.size() - 1;
+        int swaps = list.size() / 2;
+        for (int i = 0; i < swaps; i++) {
+            Integer i1 = list.get(i);
+            list.set(i, list.get(lastIndex - i));
+            list.set(lastIndex - i, i1);
+        }
+
+        System.out.println(list);
+    }
+
+    public static void reverseCourseSolution(List<Integer> list) {
+        for(int i = 0, mid=list.size() / 2, j=list.size() - 1; i < mid; i++, j--) {
+            int num1 = list.get(i);
+            int num2 = list.get(j);
+            list.set(i, num2);
+            list.set(j, num1);
+        }
+    }
+
+}
